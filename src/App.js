dc75f8aeb67c76d707e2bf06f73057c8e1a5dea9
dinssa <manny@dinssa.com>
2023-06-29 09:09:35 +0100

    Add skills dummay data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SkillList from './jsx/SkillList';
 import NewSkillForm from './jsx/NewSkillForm';
+import skills from './db';
 
 function App() {
   return (
---
+++ src/db.js
@@ -0,0 +1,23 @@
+const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'Express',
+    'MongoDB',
+    'SQL',
+    'Python',
+    'Django',
+    'Flask',
+    'PHP',
+    'Laravel',
+    'Ruby',
+    'Rails',
+    'C#',
+    'ASP.NET'
+];
+
+module.exports = {
+    skills
+}
