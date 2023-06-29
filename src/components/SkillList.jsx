4384050e4b4445293791867ce6533ab460ff8645
dinssa <manny@dinssa.com>
2023-06-29 09:59:28 +0100

    Add stylesheets

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SkillList.css";
 
 import SkillListItem from "./SkillListItem";
 
--- src/components/SkillListItem.jsx
+++ src/components/SkillListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SkillListItem.css"
 
 export default function SkillListItem({ skill }) {
   return <li>{skill}</li>;
